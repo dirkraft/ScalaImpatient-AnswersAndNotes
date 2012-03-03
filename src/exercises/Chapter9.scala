f49cadef6d83943c476d28e1200e20ad03db8972
Jason Dunkelberger <dirkraft@gmail.com>
2012-03-02 21:35:35 -0600

    Spaced tab file printing

--- src/exercises/Chapter9.scala
+++ src/exercises/Chapter9.scala
@@ -1,5 +1,6 @@
 package exercises
 
+
 object Chapter9 extends App {
 
   // 1
@@ -14,12 +15,24 @@ object Chapter9 extends App {
 
   // 2
   {
+    val n = 16
     // This solution does not truncate column contents that are longer than the column is wide (pushing will occur). 
     import io.Source
     val source = Source.fromFile("files/data.tab")
-    val lines = source.getLines.toArray
-    
-    val out = new java.io.PrintWriter("files/data.tab")
+    val out = new java.io.PrintWriter("files/data2.tab")
+    // Not writing to the same file on this one like the question asked.
+    val lines = source.getLines.toArray // all into memory
+    for (line <- lines) {
+      val tokenz = line.split("""\t""")
+      for (token <- tokenz) out.print(token + " " * (n - token.length))
+      out.println
+    }
+    out.flush()
     out.close()
+  }
+
+  // 3
+  {
+    
   }
 }
